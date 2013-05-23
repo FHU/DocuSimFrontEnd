de4c4cb707631c9237022e8eaca3dd2a87225ed0
Kenan Casey <kenancasey@gmail.com>
2013-05-23 16:15:37 -0500

    added placeholder history controller

---
+++ js/controllers/historyController.js
@@ -0,0 +1,31 @@
+
+
+
+
+docuSimApp.controller('historyController', function($scope, historyFactory){
+    $scope.histories = [];
+
+    init();
+
+    function init() {
+        $scope.histories = assessmentsFactory.getHistories();
+    }
+
+});
+
+docuSimApp.factory('historyFactory', function() {
+
+    var factory = {};
+
+    var histories = [
+        {},
+        {}, 
+    ];
+
+    factory.getHistories = function() {
+        return histories;
+    };
+
+    return factory;
+
+});
