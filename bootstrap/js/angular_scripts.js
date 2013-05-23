aaf1966ae20f5263dff933e6d18c68216534ba16
Kenan Casey <kenancasey@gmail.com>
2013-05-23 14:50:38 -0500

    Integrated angular into patients page.
    Added placeholders for new SPA structure.

--- bootstrap/js/angular_scripts.js
+++ bootstrap/js/angular_scripts.js
@@ -1,5 +1,11 @@
 var docuSimApp = angular.module('docuSim', []);
 
+
+
+
+
+
+
 //myApp.directive('myDirective', function() {});
 function MyClock($scope, $timeout) {
     $scope.time = new Date();
@@ -10,6 +16,37 @@ function MyClock($scope, $timeout) {
         },1000);
     });
 }
+
+docuSimApp.controller('patientsController', function($scope, patientsFactory){
+    $scope.patients = [];
+
+    init();
+
+    function init() {
+        $scope.patients = patientsFactory.getPatients();
+    }
+
+});
+
+docuSimApp.factory('patientsFactory', function() {
+
+    var factory = {};
+
+    var patients = [
+        { firstName: 'Carl' , lastName:'Shapiro', id: '0630', gender: 'M', age: '54', height: 69, weight: 100}, 
+        { firstName: 'Doris' , lastName:'Bowman', id: '0564', gender: 'F', age: '29', height: 66, weight: 60},
+        { firstName: 'Jennifer' , lastName:'Hoffman', id: '0321', gender: 'F', age: '33', height: 61, weight: 45},      
+        { firstName: 'Kenneth' , lastName:'Bronson', id: '2012', gender: 'M', age: '27', height: 72, weight: 74},     
+        { firstName: 'Lloyd' , lastName:'Bennett', id: '1023', gender: 'M', age: '76', height: 70, weight: 75}
+    ];
+
+    factory.getPatients = function() {
+        return patients;
+    };
+
+    return factory;
+
+});
 
 docuSimApp.controller('vitalsController', function($scope, vitalsService){
     init();
