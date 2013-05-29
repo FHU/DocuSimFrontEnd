6a714fb9a211c213a2ffffa83aba383b0f058b34
Kenan Casey <kenancasey@gmail.com>
2013-05-29 14:39:44 -0500

    added id to navbar

--- js/app.js
+++ js/app.js
@@ -83,13 +83,20 @@ docuSimApp.controller('restController', function($scope, Restangular) {
 
 
 
-docuSimApp.controller('NavbarController', function ($scope, $location) {
+docuSimApp.controller('NavbarController', function ($scope, $location, $routeParams) {
+    
+    
+
     $scope.getClass = function (path) {
         if ($location.path().substr(0, path.length) == path) {
             return true
         } else {
             return false;
         }
+    }
+
+    $scope.getID = function () {
+        return $routeParams.id;
     }
 });
 
