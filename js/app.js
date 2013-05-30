88cbe340c7a4ba143bc8502178461e07d5b36a9d
Kenan Casey <kenancasey@gmail.com>
2013-05-30 09:45:54 -0500

    added debug info

--- js/app.js
+++ js/app.js
@@ -109,9 +109,14 @@ docuSimApp.filter("localTime", function($filter) {
     
     return function(utcTime){
         //Do conversion
+
+        console.log("UTC:" + utcTime);
+
         var time_string_utc_epoch = Date.parse(utcTime);
         var localTime = new Date();
         localTime.setTime(time_string_utc_epoch);
+
+        console.log("Local:" + localTime);
 
         return localTime;
     }; 
