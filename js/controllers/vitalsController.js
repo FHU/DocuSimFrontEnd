8dab1c7e581e20efa1006f54deeeca8fa47ce330
Kenan Casey <kenancasey@gmail.com>
2013-05-30 10:02:05 -0500

    changed to push

--- js/controllers/vitalsController.js
+++ js/controllers/vitalsController.js
@@ -81,10 +81,10 @@ docuSimApp.controller('vitalsController', function($scope, $http, $resource, $ro
 		VitalsResource.create(newVital);
 		
 		//Add vital array to frontend model.
-		//newVital.TimeStamp = now;
-		//$scope.vitals.unshift(newVital);
+		newVital.TimeStamp = now;
+		$scope.vitals.push(newVital);
 
-		getPatient(patientID);
+		//getPatient(patientID);
 		//$scope.vitals = patient.Vitals(patientID);
 		$scope.clearVitals();
     }
