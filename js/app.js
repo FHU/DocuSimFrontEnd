ace77c185799c9009254966eca88b1c274281d8b
Kenan Casey <kenancasey@gmail.com>
2013-06-03 14:51:56 -0500

    added html5 mode and autoscroll

--- js/app.js
+++ js/app.js
@@ -23,6 +23,8 @@ var partialViewsUrlBase = '/partialViews/' ;
 //This configures the routes and associates each route with a view and a controller
 docuSimApp.config(function ($routeProvider, $locationProvider, RestangularProvider) {
 
+     $locationProvider.html5Mode(true);
+
     //$locationProvider.html5Mode(true); 
 
     $routeProvider
