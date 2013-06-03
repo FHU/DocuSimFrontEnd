257fd6a5eb95f10496e97960e81fa00250a9c52c
Kenan Casey <kenancasey@gmail.com>
2013-06-03 16:34:43 -0500

    trying to add html5mdoe

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ var partialViewsUrlBase = '/partialViews/' ;
 //This configures the routes and associates each route with a view and a controller
 docuSimApp.config(function ($routeProvider, $locationProvider, RestangularProvider) {
 
-    //$locationProvider.html5Mode(true); 
+    $locationProvider.html5Mode(true); 
 
     $routeProvider
         .when('/patients',
