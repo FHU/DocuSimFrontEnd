464f8b0c0c63fc6f6b31115a58d0ce50b0c34f83
Kenan Casey <kenancasey@gmail.com>
2013-06-05 11:13:04 -0500

    testing emit

--- js/controllers/vitalsController.js
+++ js/controllers/vitalsController.js
@@ -45,7 +45,7 @@ docuSimApp.controller('vitalsController', function($scope, $http, $resource, $ro
 	//Set the scope to the Vitals array in Patient
 	function onPatientReturned(patient) {
 		$scope.vitals = patient.Vitals;
-		$timeout(function() { $rootScope.$broadcast('patientUpdated', 'vitals'); });
+		$timeout(function() { $rootScope.$emit('patientUpdated'); });
 	}
 
 	function onFailure() {
