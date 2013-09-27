e373f9a76324890f569bce53827b3cd0502d6aca
Cory Dalton <cory.dalton@gmail.com>
2013-09-27 15:32:44 -0500

    fix

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-var docuSimApp = angular.module('docuSim', ['restangular', '$strap.directives', 'ng-resource']);
+var docuSimApp = angular.module('docuSim', ['restangular', '$strap.directives', 'ngResource']);
 
 //This configures the routes and associates each route with a view and a controller
 docuSimApp.config(function ($routeProvider, $locationProvider, RestangularProvider) {
