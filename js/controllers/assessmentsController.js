5500ffd4a5cab4d0d832d3c08936c49a0c412097
Cory Dalton <cory.dalton@gmail.com>
2013-10-01 22:10:09 -0500

    scroll fix test

--- js/controllers/assessmentsController.js
+++ js/controllers/assessmentsController.js
@@ -12,7 +12,9 @@ docuSimApp.controller('assessmentsController', function($scope, $location, $anch
 
     $scope.scrollTo = function(id){
         $location.hash(id);
-        $anchorScroll();
+        $(document).ready(function(){
+            $anchorScroll();
+        })
     }
 });
 
